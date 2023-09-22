8f74951f05b54802c5b239869f3cac309676fc56
Erik Mota Galindo <kenshimota@gmail.com>
2023-09-22 19:47:23 -0300

    checking case sensitive

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,5 +1,5 @@
 class Tax < ApplicationRecord
-  validates :name, presence: :true, uniqueness: { case_sensitive: true }
+  validates :name, presence: :true, uniqueness: { case_sensitive: false }
   validates :percentage, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }, uniqueness: true
 
   scope :search, ->(search) { search ? where("UPPER(name) LIKE UPPER(?)", "%#{search}%") : self }
