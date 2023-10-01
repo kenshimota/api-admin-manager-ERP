b4b6928fb0e2fe5b7a21235c50047812d6fec636
Erik Mota Galindo <kenshimota@gmail.com>
2023-10-01 12:27:35 -0300

    add validates to flieds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :tax
+
+  before_create :load_stock_and_reserved
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :bar_code, allow_blank: true, length: { minimum: 12 }
+  validates :code, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+
+  private
+
+  def load_stock_and_reserved
+    self.stock = 0
+    self.reserved = 0
+  end
 end
