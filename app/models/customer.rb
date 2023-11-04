eef1bfd079b4428b56d0dac1a12293187898400c
Erik Mota Galindo <kenshimota@gmail.com>
2023-11-04 14:55:07 -0300

    allowing filter for state and city customer and get metadata the customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,14 @@ class Customer < ApplicationRecord
   validates :last_name, presence: true, length: { minimum: 3 }
   validates :identity_document, presence: true, uniqueness: true, length: { minimum: 6 }
 
+  scope :city_id, ->(city_id) { where(city_id: city_id) if city_id }
+  scope :state_id, ->(state_id) { where(state_id: state_id) if state_id }
+  scope :metadata, lambda { |check| includes(:phones, :state, :city).joins(:state, :city).left_outer_joins(:phones) if check }
+
+  def metadata_fields
+    [:phones, :state, :city]
+  end
+
   scope :search, lambda { |search|
     if !search
       return self
