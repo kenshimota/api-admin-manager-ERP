51b1c2428402bf7c0478a7f95078f705452a4431
Erik Mota Galindo <kenshimota@gmail.com>
2024-02-21 21:59:48 -0400

    the last changed

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,7 +2,7 @@ ORDER_STATUSES_NAME = {
   pending: "pending-status",
   invoiced: "invoice-status",
   canceled: "canceled-status",
-}.freeze
+}
 
 USERS_BASE_TEST = {
   admin: {
@@ -13,12 +13,12 @@ USERS_BASE_TEST = {
     password: "managEr1.",
     email: "admin@example.com",
   },
-}.freeze
+}
 
 CURRENCIES_BASE = {
   VES: { name: "Bolivar Venezolano", exchange_rate: 1, symbol: "Bss" },
   USD: { name: "Dolar Estado Unidense", exchange_rate: 37.23, symbol: "$" },
-}.freeze
+}
 
 
 USERS_ROLES_CONST = {
@@ -26,4 +26,4 @@ USERS_ROLES_CONST = {
   customer: "customer",
   inventory: "inventory-specialist",
   sale: "sale-person",
-}.freeze
+}
